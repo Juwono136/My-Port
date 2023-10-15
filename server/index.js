c32fe056f9a8b8003ea2fa65a10fc2718b89c8f0
unknown <unosega@gmail.com>
2023-10-15 19:12:16 +0700

    update server for deploy

--- server/index.js
+++ server/index.js
@@ -7,6 +7,13 @@ import userRoutes from './routes/users.js';
 import skillRoutes from './routes/skills.js';
 import portfolioRoutes from './routes/portfolio.js';
 import cookieParser from 'cookie-parser';
+import { errorHandler } from './middleware/errorMiddleware.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express()
 dotenv.config()
@@ -19,6 +26,21 @@ app.use(cookieParser())
 app.use("/api/user", userRoutes)
 app.use("/api/skill", skillRoutes)
 app.use("/api/portfolio", portfolioRoutes)
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+
+    app.get('*', (req, res) =>
+        res.sendFile(
+            path.resolve(__dirname, '../', 'client', 'build', 'index.html')
+        )
+    );
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+}
+
+app.use(errorHandler)
 
 const CONNECTION_URL = process.env.CONNECTION_URL
 const PORT = process.env.PORT
